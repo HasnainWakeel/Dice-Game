09f98c6a94fc269e7a3c9bc54d4ebe8f6caeb682
HasnainWakeel <hasnainwakeel0317@gmail.com>
2024-10-21 21:10:06 +0500

    final

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var playerOne = prompt("Heads or Tails");
 playerOne = playerOne.toLowerCase();
 var playerTwo;
 var currentPlayer = "";
-
-// console.log(playerOne);
+var gameActive = true;
 
 while (true) {
     if (playerOne === "heads") {
@@ -11,7 +10,7 @@ while (true) {
         currentPlayer = "Pink Panther";
         break;
     } else if (playerOne === "tails") {
-        playerTwo = "Big Nose";
+        playerTwo = "heads";
         currentPlayer = "Big Nose";
         break;
     } else {
@@ -26,25 +25,82 @@ function rollDice() {
     return { diceOne: diceOne, diceTwo: diceTwo };
 }
 
-var rollButton = document.getElementById("rollButton");
-var num1Element = document.getElementsByClassName("num")[0];
-var num2Element = document.getElementsByClassName("num")[1];
-
-
 function diceRoll() {
+    if (!gameActive) {
+        return;
+    }
+
     var result = rollDice();
     var diceOne = result.diceOne;
     var diceTwo = result.diceTwo;
+
+    var num1Element = document.getElementsByClassName("num")[0];
+    var num2Element = document.getElementsByClassName("num")[1];
 
     num1Element.innerHTML = diceOne;
     num2Element.innerHTML = diceTwo;
 
     if (diceOne === 6 && diceTwo === 6) {
         alert(currentPlayer + " wins the game!");
-        if (currentPlayer === "Pink Panther") {
-            currentPlayer = "Big Nose";
-        } else {
-            currentPlayer = "Pink Panther";
-        }
-     }
-};
+        gameActive = false;
+        return;
+    }
+
+
+    if (currentPlayer === "Pink Panther") {
+        currentPlayer = "Big Nose";
+    } else {
+        currentPlayer = "Pink Panther";
+    }
+
+    alert("It's " + currentPlayer + "'s turn to roll.");
+    updateButtons(); 
+}
+
+function updateButtons() {
+    var rollButton = document.getElementById("rollButton");
+    var rollButton2 = document.getElementById("rollButton2");
+
+    if (currentPlayer === "Pink Panther") {
+        rollButton.disabled = true;
+        rollButton2.disabled = false;
+    } else {
+        rollButton.disabled = false;
+        rollButton2.disabled = true;
+    }
+}
+
+function diceRoll2() {
+    if (!gameActive) {
+        return;
+    }
+
+    var result = rollDice();
+    var diceOne = result.diceOne;
+    var diceTwo = result.diceTwo;
+
+    var num1Element2 = document.getElementsByClassName("num2")[0];
+    var num2Element2 = document.getElementsByClassName("num2")[1];
+
+    num1Element2.innerHTML = diceOne;
+    num2Element2.innerHTML = diceTwo;
+
+    if (diceOne === 6 && diceTwo === 6) {
+        alert(currentPlayer + " wins the game!");
+        gameActive = false;
+        return;
+    }
+
+    
+    if (currentPlayer === "Pink Panther") {
+        currentPlayer = "Big Nose";
+    } else {
+        currentPlayer = "Pink Panther";
+    }
+
+    alert("It's " + currentPlayer + "'s turn to roll.");
+    updateButtons(); 
+}
+
+
+updateButtons();
